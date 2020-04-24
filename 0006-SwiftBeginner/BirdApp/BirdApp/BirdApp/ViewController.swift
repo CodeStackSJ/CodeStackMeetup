7da495a0187e48ea8d17eb84af4529826466b610
jfoleysjcoe <jfoley@sjcoe.net>
2020-04-23 17:12:14 -0700

    [0006] swift xcode project

---
+++ 0006-SwiftBeginner/BirdApp/BirdApp/BirdApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirdApp
+//
+//  Created by Alexander Sosnov on 4/23/20.
+//  Copyright © 2020 Alexander Sosnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
