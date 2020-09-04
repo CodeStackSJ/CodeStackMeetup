ddcb476acb9594c46787375ec90a4996134f907d
Zack Schuster <email@zackschuster.com>
2020-09-03 18:25:51 -0700

    zip: update logging

--- zip.sh
+++ zip.sh
@@ -1,7 +1,8 @@
+echo "Creating zip files for presentation folders:"
 for x in `ls`
 do
-if [ -d $x ]; then
-	echo "Zipping ${x}/${x}..."
-	zip -qruD9 "${x}/${x}" $x -x \*.zip
+if [[ -d $x && $x =~ ^[0-9]{4}[-] ]]; then
+	echo "  ¬ $x"
+	zip -qruD9 "$x/$x" $x -x \*.zip
 fi
 done
